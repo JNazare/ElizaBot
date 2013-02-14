39818658aaa2db239a0c5b1f5746f9c147e26c18
Juliana Nazaré <jules.nazare@gmail.com>
2013-02-13 21:16:34 -0500

    working

--- app.py
+++ app.py
@@ -1,41 +1,36 @@
 import flask, flask.views
 import matcher as matcher
 import os
+from flask import session
 
 app = flask.Flask(__name__)
 
 app.secret_key = "bacon"
-count = -2
-
-def incr():
-	global count
-	count = count + 1
-	return count
 
 class View(flask.views.MethodView):
 
 	def get(self):
+		session["count"] = 0
 		return flask.render_template('index.html')
+
 
 	def post(self):
 		result = flask.request.form['expression']
-		count = incr()
+		count = session.get("count", 0)
+		session["count"] = count + 1
 		if count == 0:
 			res = matcher.hi_eliza(result)
 			flask.flash(res)
-			count = incr()
 			print count
-		elif count > 1 and count < 20:
+		elif count >= 1 and count < 10:
 			res = matcher.main(result)
 			flask.flash(res)
-			count = incr()
 			print count
-		elif count >= 20:
+		elif count >= 10:
 			res = matcher.stop_eliza()
 			flask.flash(res)
-			count = incr()
 			print count
-		return self.get()
+		return flask.render_template('index.html')
         
 
 app.add_url_rule('/', view_func=View.as_view('main'), methods=['GET', 'POST'])
